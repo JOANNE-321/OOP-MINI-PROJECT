2f5a7cd8042dd45c2957056ab1528b1748d1e33d
JOANNE CHING YIN XUAN <148308964+JOANNE-321@users.noreply.github.com>
2025-06-26 17:18:08 +0800

    Delete BookNotFoundException.java

--- BookNotFoundException.java
+++
@@ -1,8 +0,0 @@
-// BookNotFoundException.java - Custom checked exception
-// Chapter 9: Exception Handling (Custom exception)
-
-public class BookNotFoundException extends Exception {
-    public BookNotFoundException(String message) {
-        super(message);
-    }
-}
